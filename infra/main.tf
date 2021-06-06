fb4f79827c55f7cdcb4a3f80af4b461eedf87546
yuri.gomes <yuri.gomes@cazoo.com>
2021-06-06 19:03:12 +0200

    Added terraform backend as s3

--- infra/main.tf
+++ infra/main.tf
@@ -2,6 +2,14 @@ provider "aws" {
   region = "eu-central-1"
 }
 
+terraform {
+  backend "s3" {
+    bucket = "terraform-state-fante"
+    key    = "fante/terraform.tfstate"
+    region = "eu-central-1"
+  }
+}
+
 resource "aws_dynamodb_table" "highlights" {
   name         = "highlights"
   billing_mode = "PAY_PER_REQUEST"
