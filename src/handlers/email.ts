313474def10592d87dd29625a6381939b00cd808
Yuri Gomes <yurifariasg@gmail.com>
2021-11-20 20:32:32 +0100

    Removed files from old implementation and added logger

--- src/handlers/email.ts
+++ src/handlers/email.ts
@@ -1,15 +1,32 @@
-import { ScheduledEvent } from 'aws-lambda'
-
+import { Context, ScheduledEvent } from 'aws-lambda'
 import { sendEmail } from '../clients/emailClient'
 import { getStoredHighlights } from '../repositories/highlightRepository'
 import { templateFor } from '../services/template'
 import { shuffle } from '../utils'
+import pino, { PinoLambdaLogger } from 'pino-lambda'
 
-export const sendFanteEmail = async (_event: ScheduledEvent) => {
+const logger: PinoLambdaLogger = pino()
+
+export const sendFanteEmail = async (
+  event: ScheduledEvent,
+  context: Context
+) => {
+  logger.withRequest(event, context)
+
+  logger.info({ event }, 'Get stored highlights')
   const highlights = await getStoredHighlights()
 
-  const learningItems = shuffle(highlights)
+  logger.info({ highlightsCount: highlights.length }, 'Get stored highlights')
 
-  const emailContent = await templateFor(learningItems.splice(0, 5))
+  const learningItems = shuffle(highlights)
+  const selectedItems = learningItems.splice(0, 5)
+
+  logger.info({ selectedItems }, 'Selected Items')
+
+  const emailContent = await templateFor(selectedItems)
+
+  logger.info({ emailContent }, 'Email Content')
   await sendEmail(emailContent)
+
+  logger.info('Email sent')
 }
--- src/handlers/pocket.ts
+++ src/handlers/pocket.ts
@@ -1,8 +1,23 @@
 import { storeHighlights } from './../repositories/highlightRepository'
-import { fetchHighlights } from './../clients/pocketCilent'
-import { ScheduledEvent } from 'aws-lambda'
+import { fetchHighlights } from '../clients/pocketClient'
+import { Context, ScheduledEvent } from 'aws-lambda'
+import pino from 'pino-lambda'
 
-export const importHighlights = async (_event: ScheduledEvent) => {
+const logger = pino()
+
+export const importHighlights = async (
+  event: ScheduledEvent,
+  context: Context
+) => {
+  logger.withRequest(event, context)
+
+  logger.info('Import Highlights from Pocket')
+
   const highlights = await fetchHighlights()
+  logger.info({ count: highlights.length }, 'Highlights Fetched')
+
+  logger.info('Saving highlights')
   await storeHighlights(highlights)
+
+  logger.info('Highlights saved')
 }
